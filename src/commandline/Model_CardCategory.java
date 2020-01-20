d2d2299d41040fbc978b8f092f45a1e7a9c13768
JinYongjia <jinyongjia0000@gmail.com>
2020-01-20 21:11:25 +0000

    Attribute complete!

--- src/commandline/Model_CardCategory.java
+++ src/commandline/Model_CardCategory.java
@@ -1,58 +1,40 @@
 package commandline;
-
 /**
  * the categories(all 5 attribute including name and value) for every card
  */
 public class Model_CardCategory {
-	
 	private Model_Attribute[] attributes; //the 5 attributes in category
-	
-	
 	//getter and setter
 	public Model_Attribute[] getAttributes() {
 		return attributes;
 	}
-
 	public void setAttributes(Model_Attribute[] attributes) {
 		this.attributes = attributes;
 	}
-
-	
 	/*
 	 * Constructor
 	 * @param infoArray : the name of attributes in every card
 	 */
 	public Model_CardCategory(String[] infoArray) {
-		
 		for (int i = 0; i < infoArray.length; i++) {
-			
 			attributes[i] = new Model_Attribute(infoArray[i]);
-			
 		}
-	}
-	
-	
-	/**
-	 * 
-	 * @return all attributes as a list
-	 */
-	public Model_Attribute[] getAllAttributes() {
-		//TODO:
-		return null;
-	}
-
-	
+	}	
 	/**
 	 * Get the highest attribute value in this rd
 	 * @return the highest attributes, if there are several attributes, 
 	 * then return first one
 	 */
 	public Model_Attribute getHighestAttribute() {
-		//TODO:
-		return null;
+		int HighestValue = attributes[0].getValue();
+		int HighestIndex = 0;
+		for(int i = 1;i < attributes.length;i++) {
+			if(HighestValue < attributes[i].getValue()) {
+				HighestValue = attributes[i].getValue();
+				}
+		}
+		return attributes[HighestIndex];
 	}
-	
-	
 	/**
 	 * 
 	 * @param index

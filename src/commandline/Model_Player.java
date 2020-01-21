66dab1a6ac624ad6f3898587d707a04a3b31c3c6
yuuto <yuuto@172.30.208.52>
2020-01-21 16:14:19 +0000

    added score

--- src/commandline/Model_Player.java
+++ src/commandline/Model_Player.java
@@ -5,10 +5,16 @@ public class Model_Player {
 		
 	private String name;
 	private boolean isAi;
+	private int score;
+	
 	private Model_Deck playerDeck;
 	
 	
-	//Constructor
+	/**
+	 * Constructor
+	 * @param name
+	 * @param isAi
+	 */
 	public Model_Player(String name, boolean isAi) {
 		this.name = name;
 		this.isAi = isAi;
@@ -16,30 +22,32 @@ public class Model_Player {
 	}
 	
 	
-	//getter and setter
+	//Getters and setters
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public boolean isAi() {
 		return isAi;
 	}
-
 	public void setAi(boolean isAi) {
 		this.isAi = isAi;
 	}
-
 	public Model_Deck getDeck() {
 		return playerDeck;
 	}
-
 	public void setDeck(Model_Deck playerDeck) {
 		this.playerDeck = playerDeck;
 	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+
 
 	/**
 	 * check whether the player died

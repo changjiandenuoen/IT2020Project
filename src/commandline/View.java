63dd714f0d163c5ac63e211018cc1ca49d72dd3a
yuuto <yuuto@172.30.208.52>
2020-01-21 15:50:52 +0000

    updated view

--- src/commandline/View.java
+++ src/commandline/View.java
@@ -2,7 +2,35 @@ package commandline;
 
 public class View {
 	
-	public void showGameStart() {
+	private Model model;
+	private Controller controller;
+	
+	
+	/**
+	 * Constructor
+	 * @param	model
+	 * @param	controller
+	 */
+	public View(Model model, Controller controller) {
+		this.model = model;
+		this.controller = controller;
+	}
+	
+	
+	/**
+	 * Called when starting a game
+	 * TODO: 
+	 * 1. reset model
+	 * 2. choose to print past game result (get user input)
+	 * 3. game start, round one
+	 * 4. players draw card, if human alive print what you draw and how many left in your deck
+	 * 5. print category info
+	 * 6. get user input (Attribute)
+	 * 7. model.battle
+	 * 8. show round result: winner, if draw then show cards number in pile, winning card info
+	 * 9. 
+	 */
+	public void startGame() {
 		
 	}
 	

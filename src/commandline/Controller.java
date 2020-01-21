ccd878d1bb270ec27faa18f234f023aeea5518a5
yuuto <yuuto@172.30.208.52>
2020-01-21 15:50:26 +0000

    setup controller

--- src/commandline/Controller.java
+++ src/commandline/Controller.java
@@ -2,4 +2,14 @@ package commandline;
 
 public class Controller {
 
+	private Model model;
+	private View view;
+	
+	/**
+	 * Constructor
+	 * @param	model
+	 */
+	public Controller(Model model) {
+		this.model = model;
+	}
 }

8e8d8ed27ab5a97a8d5ad3556df4d2447ed4412e
JinYongjia <jinyongjia0000@gmail.com>
2020-01-22 18:28:28 +0000

    controller complete

--- src/commandline/Controller.java
+++ src/commandline/Controller.java
@@ -1,5 +1,5 @@
 package commandline;
-
+import java.util.Scanner;
 public class Controller {
 
 	private Model model;
@@ -18,7 +18,29 @@ public class Controller {
 		this.view = view;
 	}
 	
-	public void getUserInput() {
-		
+	/**
+	 * Get the choice of 1 or 2
+	 * 1: Print Game Statistics
+	 * 2: Play game
+	 */
+	public void getModeInput() {
+		Scanner s = new Scanner(System.in);
+		int choice = s.nextInt();
+		if(choice == 1) {
+			view.printGameStatistics();
+		}
+		if(choice == 2) {
+			view.startGame();
+		}
+	}
+	
+	/**
+	 * Get the number of which attribute the player choose to compare.
+	 * @return the number that the player chooses of the attribute.
+	 */
+	public int getAttributeInput() {
+		Scanner s = new Scanner(System.in);
+		int attributeNum = s.nextInt();
+		return attributeNum;
 	}
 }

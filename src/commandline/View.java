a96cfcecd58192062831a02c958c282e4bdd82c7
yuuto <yuuto@172.30.208.52>
2020-01-22 14:19:16 +0000

    added based structure

--- src/commandline/View.java
+++ src/commandline/View.java
@@ -21,14 +21,16 @@ public class View {
 	 * Called when starting a game
 	 * TODO: 
 	 * 1. reset model
-	 * 2. choose to print past game result (get user input)
+	 * 2. choose to print past game result (get user input)(controller)
 	 * 3. game start, round one
 	 * 4. players draw card, if human alive print what you draw and how many left in your deck
 	 * 5. print category info
 	 * 6. get user input (Attribute)
 	 * 7. model.battle
 	 * 8. show round result: winner, if draw then show cards number in pile, winning card info
-	 * 9. 
+	 * 9. check model gameStatus: if end to 10, else round++ to 4
+	 * 10. print end game result: winner, scores
+	 * 11. to 1
 	 */
 	public void startGame() {
 		
@@ -38,5 +40,12 @@ public class View {
 		
 	}
 	
+	/**
+	 * print Number of Games: Number of Human Wins: Number of AI Wins: 
+   	 * 		 Average number of Draws: Longest Game: 
+	 */
+	public void printGameStatistics() {
+		
+	}
 	
 }

39ec5f5fc5e2ba1e89151ba233d7f977e355d4f5
yuuto <yuuto@172.30.208.52>
2020-01-23 07:53:33 +0000

    fixed attributeIndex out of range

--- src/commandline/View.java
+++ src/commandline/View.java
@@ -1,5 +1,7 @@
 package commandline;
 
+import java.util.ArrayList;
+
 public class View {
 	
 	private Model model;
@@ -19,91 +21,126 @@ public class View {
 	
 	/**
 	 * Called when starting a game
-	 * TODO: 
-	 * 1. reset model
-	 * 2. choose to print past game result (get user input)(controller)
-	 * 3. game start, round one
-	 * 4. players draw card, if human alive print what you draw and how many left in your deck
-	 * 5. print category info
-	 * 6. get user input (Attribute)
-	 * 7. model.battle
-	 * 8. show round result: winner, if draw then show cards number in pile, winning card info
-	 * 9. check model gameStatus: if end to 10, else round++ to 4
-	 * 10. print end game result: winner, scores
-	 * 11. to 1
 	 */
 	public void startGame() {
 		
+		model.startGame();
 		
-		
-		
-		// 2.
-		this.printGameStatistics();
-		
-		System.out.println("Game Start");
-		
-		// 3.
-		while(model.getGameStatus() != 1) {
+		System.out.println("\n\nGame Start");
+
+		while(model.getGameStatus() == 0) {
 			
-			model.startRound();
+			model.roundPlusOne();
 			
 			System.out.println("Round " + model.getRound());
+			
+			ArrayList<Model_Card> desk = model.drawCard();
 			System.out.println("Round " + model.getRound() + ": Players have drawn their cards");
 			
-			if(!model.isHumanDead()) {
-				System.out.println(String.format("You drew \"%s\":", model.getPlayerTopCard(0).getName()));
-				System.out.println(model.getPlayerTopCard(0));
-				System.out.println("There are " + model.getHost().getDeck().size() + " cards in your deck");
-				System.out.println("It is your turn to select a category, the category are:");
-				this.displayCategory();
-				System.out.println("Enter the number for your attribute: ");
+			if(!model.getPlayer(0).isDead()) {
 				
-				model.setCurrAttributeIndex(controller.getUserInput());
-				
-				Model_Card winningCard = model.battle();
-				
-				// when it's a draw
-				if(winningCard == null) {
-					System.out.println("Round " + model.getRound() + 
-							": This round was a Draw, common pile now has " + model.getCommunalPile().size() + " cards");
-					
-				} else {
-					
+				Model_Card yourCard = desk.get(0);
+				for(int i = 1; i < desk.size(); i++) {
+					if(!desk.get(i).getOwner().isAI()) {
+						yourCard = desk.get(i);
+						break;
+					}
 				}
 				
+				System.out.println("You drew '" + yourCard.getName() + "':");
+				System.out.print(yourCard);
 				
+				int numCards = model.getPlayer(0).getDeck().size();
+				if(numCards == 1) {
+					System.out.println("There is one card in your deck");
+				} else {
+					System.out.println("There are " + model.getPlayer(0).getDeck().size() + " cards in your deck");
+				}
 				
-				
-				
-				model.resetModel();
+			} else {
+				System.out.println("You have Lost!");
 			}
+			
+			int attributeIndex = 0;
+			if(model.getHostIndex() == 0) {
+				System.out.println("It is your turn to select a category, the category are:");
+				this.displayCategory();
+				System.out.print("Enter the number for your attribute: ");
+				
+				attributeIndex = controller.getAttributeInput() - 1;
+			} else {
+				attributeIndex = model.getHostDeck().getTopCard().getHighestAttrIndex();
+			}
+			
+			model.setCurrAttributeIndex(attributeIndex);
+			
+			Model_Card winningCard = model.battle(desk);
+			
+			System.out.print("Round " + model.getRound() + ": ");
+			// check if it's a draw or not
+			if(winningCard == null) {
+				System.out.println("This round was a Draw, common pile now has " + model.getCommunalPile().size() + " cards");
+			} else {
+				Model_Player winner = winningCard.getOwner();
+				System.out.println("Player " + winner.getName() + " won this round");
+				System.out.println("The winning card was '" + winningCard.getName() + "':");
+				System.out.print(winningCard.toString(model.getCurrAttributeIndex()));
+			}
+			System.out.println("\n");
+			
+			this.printWhoIsWinner();
 		}
-		
-		// 4.
-		
-		
 	}
 	
-	/**
-	 * print Number of Games: Number of Human Wins: Number of AI Wins: 
-   	 * 		 Average number of Draws: Longest Game: 
-	 */
-	public void printGameStatistics() {
+	public int printChoices() {
 		
 		System.out.println("Do you want to see past results or play a game?");
 		System.out.println("\t1: Print Game Statistics");
 		System.out.println("\t2: Play game");
-		System.out.println("Enter the number for your selection: ");
+		System.out.println("\t3: Quit");
+		System.out.print("Enter the number for your selection: ");
 		
-		controller.getUserInput();
+		int choice = controller.getModeInput();
 		
-		System.out.println("\n\n");
+		System.out.println("\n");
+		
+		return choice;
+	}
+	
+	public void printGameStatistics() {
+		
+		System.out.println("\n\nGame Statistics:");
+		System.out.println("Number of Games: " + model.getNumGames());
+		System.out.println("Number of Human Wins: " + model.getNumHumanWins());
+		System.out.println("Number of AI Wins: " + model.getNumAIWins());
+		System.out.println("Average number of Draws: " + model.getAverageDraws());
+		System.out.println("Longest Game: " + model.getLongestRoundNum());
+		
+		System.out.println("\n");
 	}
 	
 	public void displayCategory() {
 		
 		for(int i = 0; i < model.category.numAttributes(); i++) {
-			System.out.println(String.format("\t%d: %s\n", i+1, model.category.getAttribute(i).getName()));
+			System.out.println(String.format("\t%d: %s", i+1, model.category.getAttribute(i).getName()));
+		}
+	}
+	
+	public void printWhoIsWinner() {
+		
+		Model_Player winner = model.whoIsWinner();
+		
+		if(winner != null) {
+			System.out.println("Game End\n");
+			System.out.println("The overall winner was " + winner.getName());
+			System.out.println("Scores:");
+			
+			for(int i = 0; i < model.numPlayers(); i++) {
+				Model_Player p = model.getPlayer(i);
+				System.out.println("\t" + p.getName() + ": " + p.getScore());
+			}
+			
+			model.resetModel();
 		}
 	}
 	

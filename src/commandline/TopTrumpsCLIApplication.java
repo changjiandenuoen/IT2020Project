9b129382142c7734bce178762dcca1a3f16a77ca
yuuto <yuuto@172.30.208.52>
2020-01-23 10:34:41 +0000

    fixed warning

--- src/commandline/TopTrumpsCLIApplication.java
+++ src/commandline/TopTrumpsCLIApplication.java
@@ -5,8 +5,6 @@ package commandline;
  */
 public class TopTrumpsCLIApplication {
 	
-	private View view;
-	
 	/**
 	 * This main method is called by TopTrumps.java when the user specifies that they want to run in
 	 * command line mode. The contents of args[0] is whether we should write game logs to a file.
@@ -26,9 +24,9 @@ public class TopTrumpsCLIApplication {
 		
 		controller.setView(view);
 		
-
-		boolean writeGameLogsToFile = false; // Should we write game logs to file?
-		if (args.length > 0 && args[0].equalsIgnoreCase("true")) writeGameLogsToFile=true; // Command line selection
+		//TODO: implement game logging
+//		boolean writeGameLogsToFile = false; // Should we write game logs to file?
+//		if (args.length > 0 && args[0].equalsIgnoreCase("true")) writeGameLogsToFile=true; // Command line selection
 		
 		// State
 		boolean userWantsToQuit = false; // flag to check whether the user wants to quit the application
@@ -36,9 +34,9 @@ public class TopTrumpsCLIApplication {
 		// Loop until the user wants to exit the game
 		while (!userWantsToQuit) {
 
-			int choice = view.printChoices();
+			view.printChoices();
 			
-			if(choice == 3) {
+			if(model.getGameStatus() == -1) {
 				userWantsToQuit=true;
 			}
 		}

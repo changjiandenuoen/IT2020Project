2468e51d3f5675a15a8785eb702e4f78e17e0202
yuuto <yuuto@172.30.208.52>
2020-02-16 16:34:39 +0000

    added parameter for async xmlhttprequest

--- resources/assets/scripts/functions.js
+++ resources/assets/scripts/functions.js
@@ -1,22 +1,16 @@
 // This is a reusable method for creating a CORS request. Do not edit this.
-function createCORSRequest(method, url) {
+function createCORSRequest(method, url, async) {
     var xhr = new XMLHttpRequest();
     if ("withCredentials" in xhr) {
-
-        // Check if the XMLHttpRequest object has a "withCredentials" property.
-        // "withCredentials" only exists on XMLHTTPRequest2 objects.
-        xhr.open(method, url, false);
-
+        if(async) {
+            xhr.open(method, url, async);
+        } else {
+            xhr.open(method, url, false);
+        }
     } else if (typeof XDomainRequest != "undefined") {
-
-        // Otherwise, check if XDomainRequest.
-        // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
         xhr = new XDomainRequest();
         xhr.open(method, url);
-
     } else {
-
-        // Otherwise, CORS is not supported by the browser.
         xhr = null;
     }
 
